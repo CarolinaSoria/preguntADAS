be4cb3f0225230c36c964f5af82dcebc9801445c
CarolinaSoria <carolinasoria49@msn.com>
2019-06-02 19:39:24 -0300

    fix del login

--- js/login.js
+++ js/login.js
@@ -2,8 +2,8 @@ let button = document.getElementById("button1");
 button.onclick = function validate() {
     let username = document.getElementById("username").value;
     let password = document.getElementById("password").value;
-    if (username == "Ada" && password == "2019") {
-        alert(`Bienvenida ${username}`);
+    if (username == "HPotter" && password == "hagrid") {
+        alert(`Bienvenidx ${username}`);
 // PREGUNTAR POR: window.location = "success.html"; // Redirecting to other page.
     } else {
         alert("Login incorrecto. Intente nuevamente");
