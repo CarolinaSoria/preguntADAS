939099c78c2cacf226c3b45c72d0774973147df6
Lerita <valeriam@gmail.com>
2019-06-03 12:08:28 -0300

    nuevas funciones

--- js/perfil.js
+++ js/perfil.js
@@ -28,16 +28,17 @@ const mostrarDesplegable = function(numeroTitulo) {
     
 // }
 
-let user = ["Nat", "Carol", "Gamora", "Nebula", "Wanda", "Peggy", "Hill", "Pepper", "Okoye"];
-let level = ["2", "3", "5", "4", "1", "3", "5", "1", "2"];
-let score = [70, 150, 4000, 3200, 10, 180, 4200, 21, 75];
-let followers = ["Wanda", "Peggy", "Hill"];
-let following = ["Carol", "Gamora", "Nebula", "Pepper", "Okoye"];
-let userQuestions = ["Pregunta 1", "Pregunta 2", "Pregunta 3"];
-let questionsWon = ["35/74"];
-let challengesWon = ["4/6"];
-let versusWon = "7/9";
-
+let user = {
+    name: "Nat",
+    level: 4,
+    score: 2500,
+    followers: ["Wanda", "Peggy", "Hill"],
+    following: ["Carol", "Gamora", "Nebula", "Pepper", "Okoye"],
+    questions: ["Pregunta 1", "Pregunta 2", "Pregunta 3"],
+    questionsWon: "35/73",
+    challengesWon: "4/6",
+    versusWon: "7/9"
+}
 
 const mostrarFollow = function(follow) {
     for(i = 0; i < follow.length; i++)
@@ -46,17 +47,17 @@ const mostrarFollow = function(follow) {
 
 const obtenerCantFollow = function() {
     let cantidad = document.getElementById("followers");
-    cantidad.innerHTML = `<span id="followers" onclick="mostrarFollow(followers)">${followers.length} Seguidores</span>
-    | <span onclick="mostrarFollow(following)">${following.length} Siguiendo</span>`;
+    cantidad.innerHTML = `<span id="followers" onclick="mostrarFollow(user.followers)">${user.followers.length} Seguidores</span>
+    | <span onclick="mostrarFollow(user.following)">${user.following.length} Siguiendo</span>`;
 }
 obtenerCantFollow();
 
 const obtenerCantidades = function() {
     let estadisticas = document.getElementById("title_1");
-    estadisticas.innerHTML = `<p>Preguntas respondidas: ${questionsWon[0]}<br />
+    estadisticas.innerHTML = `<p>Preguntas respondidas: ${user.uestionsWon[0]}<br />
     Preguntas respondidas por categoría:<br />
-    Competencias ganadas: ${versusWon}<br />
-    Desafíos ganados: ${challengesWon[0]}</p>`;
+    Competencias ganadas: ${user.versusWon}<br />
+    Desafíos ganados: ${user.challengesWon[0]}</p>`;
 }
 obtenerCantidades();
 
@@ -76,8 +77,8 @@ obtenerCantidades();
 
 
 const mostrarPreguntas = function() {
-    for(let i = 0; i < userQuestions.length; i++) {
-    console.log(`${userQuestions[i]}`);
+    for(let i = 0; i < user.questions.length; i++) {
+    console.log(`${user.questions[i]}`);
     // let algo = document.getElementById('preguntas');
     // algo.innerHTML = preguntasSubidas;
  }
@@ -85,7 +86,7 @@ const mostrarPreguntas = function() {
 
 const obtenerCantPreguntas = function() {
     let cantPreguntas = document.getElementById("preguntas-subidas");
-    cantPreguntas.innerHTML = `<p onclick="mostrarPreguntas()">Preguntas Subidas (${userQuestions.length})</p>`;
+    cantPreguntas.innerHTML = `<p onclick="mostrarPreguntas()">Preguntas Subidas (${user.questions.length})</p>`;
 }
 obtenerCantPreguntas();
 
