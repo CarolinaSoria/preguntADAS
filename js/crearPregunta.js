593fa579e0525ef6f77625b25839dc8e805d207c
CarolinaSoria <carolinasoria49@msn.com>
2019-06-03 12:59:45 -0300

    nuevas funciones que muestran las respuestas, clear all

--- js/crearPregunta.js
+++ js/crearPregunta.js
@@ -2,17 +2,38 @@ let button = document.getElementById("btn2");
 
 function getQuestion(){
     let inputQuestion = document.getElementById("question").value;
-    console.log(inputQuestion);
+   
 }
 
 function getAnswers(){
+    let answers = [];
     for(i = 1; i < 5; i++){
         let inputAnswer = document.getElementById(`answer-${i}`).value;
-        console.log(inputAnswer);
-    } 
+        answers.push(inputAnswer);
+    }
+     return answers;
 }
 function displayInputs(){
     getQuestion();
-    getAnswers();
+   let answers = getAnswers(); 
+    answersContainer = document.getElementById("answers-container");
+    answersContainer.innerHTML =
+    ` <p>Cual de todas las respuestas es la correcta?</p> `
+    for(i = 0; i < 4; i++){
+       answersContainer.innerHTML += `
+       <input type="radio" name="answer" value="1"> ${answers[i]}`;
+
+    }
+
+}
+
+function clearAll(){
+    let inputElements = document.getElementsByType("radio");
+    for (let i=0; i < inputElements.length; i++) {
+        if (inputElements[i].type == "text") {
+            inputElements[i].value = "";
+            answersContainer.innerHTML = "";
+        }
+    }
     
 }
